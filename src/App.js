1fce0a3ab8f587e851e531ab0c9527a19c3cb01f
Momen Fnnana <mommen@hemma.sa>
2022-10-12 09:56:36 +0300

    replace inline navigation with external navigation file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 import React from "react";
-import { Pressable } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { onBordingScreen } from "./screens";
-import { useNavigation } from "@react-navigation/native";
-import BottomTabs from "./navigator/BottomTabs";
-
-const Stack = createNativeStackNavigator();
+import MainStack from "./navigator/MainStack";
 
 const AppIndex = () => {
-  const { goBack, canGoBack } = useNavigation();
-  return (
-    <Stack.Navigator
-      initialRouteName="OnBordingScreen"
-      screenOptions={{
-        headerLeft: () =>
-          canGoBack() && (
-            <Pressable onPress={goBack}>
-              <FontAwesome5
-                name="long-arrow-alt-left"
-                size={24}
-                color="black"
-              />
-            </Pressable>
-          ),
-      }}
-    >
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="OnBordingScreen"
-        component={onBordingScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="BottomStack"
-        component={BottomTabs}
-      />
-    </Stack.Navigator>
-  );
+  return <MainStack />;
 };
 
 export default AppIndex;
